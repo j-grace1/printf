bfac070fc1fa5e500c83615f26b91d45c2ad9ca1
j-grace1 <nchidejoshoua@gmail.com>
2023-03-25 11:48:31 +0100

    changed code structure

--- project_0.c
+++ project_0.c
@@ -1,104 +1,56 @@
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-int _strlen(char *s)
-{
-    int len = 0;
-    while (*s++)
-        len++;
-    return len;
-}
-
-void _print_number(int n)
-{
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10) {
-        _print_number(n / 10);
-    }
-
-    _putchar((n % 10) + '0');
-}
-
-void _print_string(char *str)
-{
-    while (*str) {
-        _putchar(*str++);
-    }
-}
-
+/**
+ * _printf - prints output according to a format.
+ * @format: format string.
+ *
+ * Return: the number of characters printed (excluding the null byte used to
+ * end output to strings).
+ */
 int _printf(const char *format, ...)
 {
+    int count = 0;
     va_list args;
+    int i, j;
+    char *str_arg;
+
     va_start(args, format);
 
-    int count = 0;
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
+    for (i = 0; format[i]; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+
+            switch (format[i])
+            {
                 case 'c':
-                    _putchar(va_arg(args, int));
+                    putchar(va_arg(args, int));
                     count++;
                     break;
                 case 's':
-                    _print_string(va_arg(args, char *));
-                    count += _strlen(va_arg(args, char *));
-                    break;
-                case 'd':
-                case 'i':
-                    _print_number(va_arg(args, int));
-                    break;
-                case 'o':
-                    _print_string("0");
-                    _print_number(va_arg(args, unsigned int));
-                    break;
-                case 'x':
-                case 'X':
+                    str_arg = va_arg(args, char *);
+                    if (str_arg)
                     {
-                        char *hex_digits = "0123456789abcdef";
-                        char buf[32] = { 0 };
-                        int n = va_arg(args, unsigned int);
-                        int i = 30;
-                        while (n) {
-                            buf[i--] = hex_digits[n % 16];
-                            n /= 16;
+                        for (j = 0; str_arg[j]; j++)
+                        {
+                            putchar(str_arg[j]);
+                            count++;
                         }
-                        if (*format == 'X') {
-                            for (int j = i + 1; j <= 30; j++) {
-                                if (buf[j] >= 'a' && buf[j] <= 'f') {
-                                    buf[j] = buf[j] - 32;
-                                }
-                            }
-                        }
-                        _print_string(buf + i + 1);
-                        count += _strlen(buf + i + 1);
                     }
                     break;
                 case '%':
-                    _putchar('%');
+                    putchar('%');
                     count++;
                     break;
-                default:
-                    _putchar('%');
-                    _putchar(*format);
-                    count += 2;
-                    break;
             }
-        } else {
-            _putchar(*format);
+        }
+        else
+        {
+            putchar(format[i]);
             count++;
         }
-        format++;
     }
 
     va_end(args);
