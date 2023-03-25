29bb4718ffc2965ab06a24941ed6c2329d276853
j-grace1 <nchidejoshoua@gmail.com>
2023-03-25 12:40:04 +0100

    made code betty-style compliant

--- project_0.c
+++ project_0.c
@@ -10,50 +10,50 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    int i, j;
-    char *str_arg;
+	int count = 0;
+	va_list args;
+	int i, j;
+	char *str_arg;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
 
-            switch (format[i])
-            {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    str_arg = va_arg(args, char *);
-                    if (str_arg)
-                    {
-                        for (j = 0; str_arg[j]; j++)
-                        {
-                            putchar(str_arg[j]);
-                            count++;
-                        }
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(format[i]);
-            count++;
-        }
-    }
+			switch (format[i])
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+ 					break;
+				case 's':
+ 					str_arg = va_arg(args, char *);
+					if (str_arg)
+					{
+						for (j = 0; str_arg[j]; j++)
+						{
+							putchar(str_arg[j]);
+							count++;
+						}
+ 					}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return count;
+	return count;
 }
