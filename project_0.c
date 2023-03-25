3146fc1e4afa7cd95bc3cde31f77844e2637f1a8
j-grace1 <nchidejoshoua@gmail.com>
2023-03-25 12:06:45 +0100

    modified code

--- project_0.c
+++ project_0.c
@@ -1,52 +1,59 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
+/**
+ * _printf - prints output according to a format.
+ * @format: format string.
+ *
+ * Return: the number of characters printed (excluding the null byte used to
+ * end output to strings).
+ */
 int _printf(const char *format, ...)
 {
+    int count = 0;
     va_list args;
+    int i, j;
+    char *str_arg;
+
     va_start(args, format);
 
-    int count = 0;
-
-    for (int i = 0; format[i] != '\0'; i++) {
-        if (format[i] == '%') {
+    for (i = 0; format[i]; i++)
+    {
+        if (format[i] == '%')
+        {
             i++;
 
-            switch (format[i]) {
-                case 'c': {
-                    char c = (char) va_arg(args, int);
-                    putchar(c);
+            switch (format[i])
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
                     count++;
                     break;
-                }
-
-                case 's': {
-                    char *s = va_arg(args, char *);
-                    if (s == NULL) {
-                        s = "";
+                case 's':
+                    str_arg = va_arg(args, char *);
+                    if (str_arg)
+                    {
+                        for (j = 0; str_arg[j]; j++)
+                        {
+                            putchar(str_arg[j]);
+                            count++;
+                        }
                     }
-                    printf("%s", s);
-                    count += strlen(s);
                     break;
-                }
-
-                case '%': {
+                case '%':
                     putchar('%');
                     count++;
                     break;
-                }
-
-                default: {
-                    printf("Error: invalid format specifier %%%c\n", format[i]);
-                    return -1;
-                }
             }
-        } else {
+        }
+        else
+        {
             putchar(format[i]);
             count++;
         }
     }
 
     va_end(args);
+
     return count;
 }
