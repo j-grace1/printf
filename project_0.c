f9a09dc419bcc4379f4fb1d028ca8a21b3187d43
j-grace1 <nchidejoshoua@gmail.com>
2023-03-25 11:22:53 +0100

    modified the _printf function

--- project_0.c
+++ project_0.c
@@ -1,109 +1,58 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * main - Entry point
+ * _printf - prints output according to a format
+ * @format: the format string
  *
- * Return: Always 0
- */
-int main(void)
-{
-    int len;
-
-    len = _printf("Let's print a simple sentence.\n");
-    _printf("Length of string: %d\n", len);
-    return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-/**
- * _printf - produces output according to a format
- * @format: a character string
- *
- * Return: the number of characters printed
+ * Return: the number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-    int i, count = 0;
     va_list args;
-    char *str_arg;
-    char buffer[1024];
+    int count = 0;
 
     va_start(args, format);
-    for (i = 0; format[i]; i++)
+
+    while (*format != '\0')
     {
-        if (format[i] == '%')
+        if (*format == '%')
         {
-            i++;
-            switch (format[i])
+            format++;
+
+            switch (*format)
             {
-            case 's':
-                str_arg = va_arg(args, char *);
-                count += _print_str(str_arg, buffer);
-                break;
-            case '%':
-                _putchar('%');
-                count++;
-                break;
-            default:
-                _putchar('%');
-                _putchar(format[i]);
-                count += 2;
-                break;
+                case 'c':
+                    count += putchar(va_arg(args, int));
+                    break;
+
+                case 's':
+                    count += printf("%s", va_arg(args, char *));
+                    break;
+
+                case '%':
+                    count += putchar('%');
+                    break;
+
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
             }
         }
         else
         {
-            _putchar(format[i]);
-            count++;
+            count += putchar(*format);
         }
+
+        format++;
     }
+
     va_end(args);
 
-    write(1, buffer, _strlen(buffer));
-    return (count);
+    return count;
 }
 
-/**
- * _print_str - prints a string to a buffer
- * @str: the string to print
- * @buffer: the buffer to print to
- *
- * Return: the number of characters printed
- */
-int _print_str(char *str, char *buffer)
-{
-    int i, len;
 
-    len = _strlen(str);
-    for (i = 0; i < len; i++)
-        buffer[i] = str[i];
-
-    return (len);
-}
-
-/**
- * _strlen - calculates the length of a string
- * @str: the string to measure
- *
- * Return: the length of the string
- */
-int _strlen(char *str)
-{
-    int i = 0;
-
-    while (str[i])
-        i++;
-
-    return (i);
-}
